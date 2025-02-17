402b66c9494913c57d9e20139282c01775b26c6a
Akito0723 <519226491@qq.com>
2025-02-17 17:48:23 +0800

    feat: astrbot_plugin_mofish 调试

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # astrbot_plugin_mofish
 
-astrbot_plugin_mofish
+# 摸鱼
+认认真真地上班，根本不叫赚钱，这是劳动换取报酬。只有偷懒，在上班的时候浑水摸鱼，那才是从你老板那赚到了钱。
+- `/mofish today` 还有几天放假!
+- `/mofish auto` 自动在本机时间 9 点发送 momoyu热榜
+  - 你需要确保你的机器的时区是 UTC+8 的时区，否则时间可能并不是 9 点。
+- `/mofish help` 帮助信息
 
+--- 
 # 支持
-
 [帮助文档](https://astrbot.soulter.top/dev/plugin.html)
--- main.py
+++ main.py
@@ -2,16 +2,34 @@ import logging
 from .holiday import Holiday
 from astrbot.api.all import Context, AstrMessageEvent, CommandResult
 
+
 class Main:
     def __init__(self, context: Context) -> None:
         self.NAMESPACE = "astrbot_plugin_mofish"
         self.context = context
         self.logger = logging.getLogger("astrbot")
-        self.context.register_commands(self.NAMESPACE, "mofish", "今日摸鱼", 1, self.today_info_desc)
+        self.context.register_commands(self.NAMESPACE, "mofish", "每日摸鱼", 1, self.mofish)
         self.holiday_process = Holiday()
 
+    async def mofish(self, event: AstrMessageEvent, context: Context):
+        args = event.message_str.split(" ")
+        help_msg = '\n'.join(['每日摸鱼 指令描述',
+                              '/mofish today 今日信息'
+                              '/mofish auto 启动/关闭每日 9 点发送摸鱼信息(锐意开发中)'])
+        if len(args) < 2:
+            return CommandResult().message(help_msg).use_t2i(False)
+        if args[1] == "today":
+            self.today_info_desc(event)
+        elif args[1] == "auto":
+            self.auto_send_mofish_info(event)
 
+        elif args[1] == "help":
+            return CommandResult().message(help_msg).use_t2i(False)
+        else:
+            return CommandResult().message(help_msg).use_t2i(False)
 
     async def today_info_desc(self, event: AstrMessageEvent):
         yield event.plain_result('\n'.join(self.holiday_process.getTodayDesc()))
-        # yield event.image_result(url)
+
+    async def auto_send_mofish_info(self, event: AstrMessageEvent):
+        yield event.plain_result('锐意开发中')
